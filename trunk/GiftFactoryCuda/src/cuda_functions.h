ca73d56bf35e1e331a6eba60abff7c661d32e681
celine.cogny <celine.cogny@369dbe5e-add6-1733-379f-dc396ee97aaa>
2011-12-02 19:36:42 +0000

    Création de la classe SnowManager dans cuda_functions.h

--- trunk/GiftFactoryCuda/src/cuda_functions.h
+++ trunk/GiftFactoryCuda/src/cuda_functions.h
@@ -1,8 +1,29 @@
-#ifndef CONVOLUTIONSEPARABLE_COMMON_H
-#define CONVOLUTIONSEPARABLE_COMMON_H
+#ifndef __CUDA_FUNCTION_H__
+#define __CUDA_FUNCTION_H__
 
 #include <iostream>
+#include <vector>
 
+typedef struct _flake
+{
+	int x;
+	int y;
+	int size;
+} Flake;
+
+class SnowManager
+{
+	private :
+		unsigned int m_uiNbFlakes;
+		int m_iXMax;
+		int m_iYMax;
+		int m_iZMax;
+		Flake * m_vFlakes;
+	public:
+		SnowManager(const unsigned int & nbFlakes, const int & xMax, const int & yMax, const int & zMax);
+		void update();
+		void see();
+};
 void launch_bb();
 
 #endif
