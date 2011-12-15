6964709577d57a54e67a7100e7f31882ed771f6d
delau.eleonore <delau.eleonore@369dbe5e-add6-1733-379f-dc396ee97aaa>
2011-12-15 21:27:47 +0000

    Ajout d'une maison avec une guirlande lumineuse changant de couleur au cours du temps

--- trunk/GiftFactoryCuda/src/Object.cpp
+++ trunk/GiftFactoryCuda/src/Object.cpp
@@ -72,8 +72,13 @@ void Object::init()
 			
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_NEAREST);
 			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_NEAREST);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
-			glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			if(objFileName == "./objects/terrain.obj" || objFileName == "../objects/terrain.obj" ){
+				glTexParameteri( GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_REPEAT );
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_REPEAT);
+			}else{
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+				glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
+			}
 			glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, tex->texPicture->w, tex->texPicture->h, 0, GL_RGB, GL_UNSIGNED_BYTE, tex->texPicture->pixels);
 
 		}
